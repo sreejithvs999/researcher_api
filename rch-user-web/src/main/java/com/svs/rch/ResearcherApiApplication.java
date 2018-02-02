9ebff05e404ab02d9518c71bd0ac32ab8a1978bf
sreejith.vs <Sreejith VS@DESKTOP-CJGB11I>
2018-02-02 10:46:16 +0530

    project structure init.

---
+++ rch-user-web/src/main/java/com/svs/rch/ResearcherApiApplication.java
@@ -0,0 +1,14 @@
+package com.svs.rch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.svs.rch.ResearcherApiApplication;
+
+@SpringBootApplication
+public class ResearcherApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearcherApiApplication.class, args);
+	}
+}
