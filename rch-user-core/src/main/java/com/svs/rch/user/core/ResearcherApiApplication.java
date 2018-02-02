497ffdf1d20b510462c392e94ff4bfa00d1f6647
sreejith.vs <Sreejith VS@DESKTOP-CJGB11I>
2018-02-02 10:46:16 +0530

    project structure init.

---
+++ rch-user-core/src/main/java/com/svs/rch/user/core/ResearcherApiApplication.java
@@ -0,0 +1,12 @@
+package com.svs.rch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearcherApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearcherApiApplication.class, args);
+	}
+}
---
+++ rch-user-web/src/main/java/com/svs/rch/user/web/ResearcherApiApplication.java
@@ -0,0 +1,27 @@
+package com.svs.rch.user.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@Configuration
+@SpringBootApplication
+public class ResearcherApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearcherApiApplication.class, args);
+	}
+	
+	@Bean
+	public MessageSource messageSource() {
+		
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:i18n/messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		
+		return messageSource;
+	}
+}
