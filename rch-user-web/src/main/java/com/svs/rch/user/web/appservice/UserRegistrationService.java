799e117954c0d6de8e9ea77a88c58c97b2e13505
sreejith.vs <Sreejith VS@DESKTOP-CJGB11I>
2018-02-05 17:08:10 +0530

    response format chnages

---
+++ rch-user-web/src/main/java/com/svs/rch/user/web/appservice/UserRegistrationService.java
@@ -0,0 +1,9 @@
+package com.svs.rch.user.web.appservice;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserRegistrationService {
+
+	
+}
---
+++ rch-user-web/src/main/java/com/svs/rch/user/web/beans/UserRegisterResponse.java
@@ -0,0 +1,5 @@
+package com.svs.rch.user.web.beans;
+
+public class UserRegisterResponse {
+
+}
